b99477bac45d37f76a727084cdab2642eee586ec
artisyn <artioms1@gmail.com>
2022-03-29 21:33:34 +0300

    Intro

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import './App.scss';
+import React, { useState } from 'react';
 
 function App() {
-	return <div className="App"></div>;
+	const [value, setValue] = useState(0);
+	const [text, setText] = useState('Text');
+	return (
+		<div className="App">
+			<h1>{value}</h1>
+			<h1>{text}</h1>
+			<input
+				type="text"
+				name=""
+				id=""
+				// value={text}
+				onChange={(e) => {
+					setText(e.target.value);
+				}}
+			/>
+			<button
+				onClick={() => {
+					setValue(value + 1);
+				}}
+			>
+				Increment
+			</button>
+			<button
+				onClick={() => {
+					setValue(value - 1);
+				}}
+			>
+				Decrement
+			</button>
+		</div>
+	);
 }
 
 export default App;
